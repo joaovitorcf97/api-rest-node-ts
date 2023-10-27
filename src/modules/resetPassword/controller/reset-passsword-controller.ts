4e56800987ac7257ce004e426609d38ba6c78394
João Vitor Costa Ferreira <joaovitorcfdev@gmail.com>
2023-10-27 19:16:11 -0300

    feat: adding validateUser function

--- src/modules/resetPassword/controller/reset-passsword-controller.ts
+++ src/modules/resetPassword/controller/reset-passsword-controller.ts
@@ -1,7 +1,37 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
+import { StatusErrorsEnum } from '../../../enum/status.enum';
+import { ZodEnum } from '../../../enum/zod.enum';
+import { resetPasswordService } from '../service/reset-password-service';
 
 class ResetPasswordController {
-  public async validateUser(request: Request, response: Response) {}
+  public async validateUser(request: Request, response: Response) {
+    const email = request.body.email;
+
+    try {
+      const ZUserSchema = z
+        .string()
+        .email({ message: `Email ${ZodEnum.REQUIRED}` });
+
+      ZUserSchema.parse(email);
+    } catch (error: any) {
+      return response.status(400).json({
+        message: StatusErrorsEnum.E400,
+        error: error.erros,
+      });
+    }
+
+    try {
+      return response.json({
+        message: 'Codigo enviado para o e-mail',
+        data: await resetPasswordService.validateUser(email),
+      });
+    } catch (error: any) {
+      return response.status(404).json({
+        message: error.message,
+      });
+    }
+  }
 
   public async validateSecurityCode(request: Request, response: Response) {}
 
--- src/modules/resetPassword/service/reset-password-service.ts
+++ src/modules/resetPassword/service/reset-password-service.ts
@@ -1,5 +1,37 @@
+import { StatusErrorsEnum } from '../../../enum/status.enum';
+import { prismaConnnect } from '../../../prismaConn';
+
 class ResetPasswordService {
-  public async validateUser() {}
+  public async validateUser(email: string) {
+    const findUser = await prismaConnnect.user.findUnique({
+      where: { email },
+      include: { reset_password_secret: true },
+    });
+
+    if (!findUser) {
+      throw new Error(StatusErrorsEnum.E404);
+    }
+
+    if (!findUser.reset_password_secret) {
+      const generateSecret = Number(
+        Array.from({ length: 6 }, () => Math.floor(Math.random() * 9)).join(''),
+      );
+
+      const { secret } = await prismaConnnect.resetPasswordSecret.create({
+        data: {
+          secret: generateSecret,
+          userId: findUser.id,
+        },
+        select: {
+          secret: true,
+        },
+      });
+
+      return { email, secret };
+    }
+
+    return { email, secret: findUser.reset_password_secret.secret };
+  }
 
   public async validateSecurityCode() {}
 
